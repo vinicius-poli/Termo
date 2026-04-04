4f7db62d28a908e65584636dd1887d0e0e4be09d
vinicius.poli <alemaumvp@hotmail.com>
2026-04-04 15:03:54 -0300

    Adicionado o sorteio de palavras aleatórias

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace Termo.ConsoleApp;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] palavras = [
+            "IDEIA"
+        ];
+
+        int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
+
+        string palavraAleatoria = palavras[indiceAleatorio];
+
+        char[] letrasAcertadas = new char [palavraAleatoria.Length];
+
+        for (int i = 0; i < palavraAleatoria.Length; i++)
+        {
+            letrasAcertadas[i] = '*';
+        }
+        Console.WriteLine(letrasAcertadas);     
+        Console.WriteLine(palavraAleatoria);
+
+        Console.Write("Digite uma letra: ");
+        Console.ReadLine();     
     }
 }
